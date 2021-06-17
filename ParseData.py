fc909d863ef858aed4b9d236ae41ad59adea47f1
johanneswittrock <johanneswittrock@gmx.de>
2021-06-18 00:11:14 +0200

    fit algorithm for every lp

--- ParseData.py
+++ ParseData.py
@@ -1,51 +1,49 @@
 import rdflib
-from ontolearn import KnowledgeBase,SampleConceptLearner, Refinement
-from ontolearn.metrics import F1, PredictiveAccuracy, CELOEHeuristic,DLFOILHeuristic
 
+from ontolearn import KnowledgeBase
+from ontolearn.concept_learner import CustomConceptLearner
+from ontolearn.heuristics import DLFOILHeuristic
 
 g = rdflib.Graph()
 g.parse("kg-mini-project-train.ttl", format="turtle")
-
 print(len(g))
-for subject, predicate, object in g:
-    print(subject)
-    print(predicate)
-    print(object)
-    break
-
-#select positive (i think include resources are positive examples and exclude resources negative) examples for the first learning problem
-qres = g.query(
-    """SELECT ?ex 
-    WHERE {<https://lpbenchgen.org/resource/lp_4> <https://lpbenchgen.org/property/includesResource> ?ex .}"""
-)
-positiveExamples = set()
-for row in qres:
-    ex = str(row.ex)
-    positiveExamples.add(ex)
-
-
-qres = g.query(
-    """SELECT ?ex 
-    WHERE {<https://lpbenchgen.org/resource/lp_4> <https://lpbenchgen.org/property/excludesResource> ?ex .}"""
-)
-negativeExamples = set()
-for row in qres:
-    ex = str(row.ex)
-    negativeExamples.add(ex)
 
 kb = KnowledgeBase(path='carcinogenesis.owl')
-rho = Refinement(kb)
-#for refs in enumerate(rho.refine(kb.thing)):
- #   print(refs)
 
-model = SampleConceptLearner(knowledge_base=kb,
-                             quality_func=F1(),
-                             terminate_on_goal=True,
-                             heuristic_func=DLFOILHeuristic(),
-                             iter_bound=100,
-                             verbose=False)
+i = 1
+while i <= 25:
 
-#does not work yet, i dont know why that error occurs
-model.predict(pos=positiveExamples, neg=negativeExamples)
-model.show_best_predictions(top_n=10)
+    #select positive (i think include resources are positive examples and exclude resources negative) examples for the first learning problem
+    qres = g.query(
+        "SELECT ?ex WHERE {<https://lpbenchgen.org/resource/lp_" + str(i) + "> <https://lpbenchgen.org/property/includesResource> ?ex .}"
+    )
+    positiveExamples = set()
+    for row in qres:
+        ex = str(row.ex)
+        positiveExamples.add(ex)
 
+
+    qres = g.query(
+        "SELECT ?ex WHERE {<https://lpbenchgen.org/resource/lp_" + str(i) + "> <https://lpbenchgen.org/property/excludesResource> ?ex .}"
+    )
+    negativeExamples = set()
+    for row in qres:
+        ex = str(row.ex)
+        negativeExamples.add(ex)
+
+    model = CustomConceptLearner(knowledge_base=kb, heuristic_func=DLFOILHeuristic())
+
+    model.fit(pos=positiveExamples, neg=negativeExamples)
+    hypotheses = model.best_hypotheses(n=1)
+
+    ## vlt in der grading phase die individuals die missing carcinogenesis
+    predictions = model.predict(individuals=list(positiveExamples) + list(negativeExamples), hypotheses=hypotheses)
+
+    print("Learning Problem: " + str(i))
+    print(predictions)
+
+    i = i + 1
+
+
+##die negativen sind nur teilweise da, muss die restlichen aus carsinogenesis holen, alle außer die die in den positiven sind
+##
